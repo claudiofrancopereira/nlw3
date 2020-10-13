cd0f22c826c55068b68f0f97d919f2d7ece4c970
Claudio Pereira <claudiofrancopereira@gmail.com>
2020-10-13 15:05:53 -0300

    Added MAP to Orphanages-map

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Map, TileLayer } from 'react-leaflet';
+
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi'
 
 import mapMarkerImg from '../images/map-maker.svg';
 
+import 'leaflet/dist/leaflet.css';
 import '../styles/pages/orphanages-map.css';
 
 function OrphanagesMap() {
@@ -22,12 +25,22 @@ function OrphanagesMap() {
                     <span>São Paulo</span>
                 </footer>
             </aside>
-            
+                        
+            <Map
+                center={[-21.1258039,-48.9769468]}
+                zoom={15}
+                style={{ width: '100%', height: '100%' }}
+            >
+                {/*<TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />*/}
+                <TileLayer 
+                    url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                />
+            </Map>
+
             <Link to="" className="create-orphanage">
                 <FiPlus size={32} color="#FFF" />
             </Link>
 
-            <div></div>
         </div>
     );
 }
